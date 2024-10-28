8456239aea36a9e20c91858c7f7aab3f3c182c88
Ivanka2007 <ivankacicerska@gmail.com>
2024-10-28 13:11:40 +0200

    Add files via upload

---
+++ Lab_4.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    double S;
+
+    cout << "i = "; cin >> i;
+    S = 0;
+    i = 1;
+    //1
+    while (i <= 15)
+    {
+        S += ((sin(10) * i) + (cos(10) / i)) / sqrt(i);
+        i++;
+    }
+    cout << S << endl;
+    //2
+    S = 0;
+    i = 1;
+    do {
+        S += ((sin(10) * i) + (cos(10) / i)) / sqrt(i);
+        i++;
+    } while (i <= 15);
+    cout << S << endl;
+    //3
+    S = 0;
+    for (i = 1; i <= 15; i++)
+    {
+        S += ((sin(10) * i) + (cos(10) / i)) / sqrt(i);
+    }
+    cout << S << endl;
+    return 0;
+}
